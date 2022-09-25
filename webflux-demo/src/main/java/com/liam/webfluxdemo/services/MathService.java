a0003919f8024ef33d9f6f0aec53db772146de80
Liam Desmond <liamjdesmond84@gmail.com>
2022-09-25 16:15:02 -0500

    Adding multiplicationTable method

--- webflux-demo/src/main/java/com/liam/webfluxdemo/services/MathService.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/services/MathService.java
@@ -1,5 +1,9 @@
 package com.liam.webfluxdemo.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.stereotype.Service;
 
 import com.liam.webfluxdemo.dtos.Response;
@@ -10,6 +14,14 @@ public class MathService {
 	public Response findSquare(int input) {
 		
 		return new Response(input * input);
+	}
+	
+	public List<Response> multiplicationTable(int input) {
+		
+		return IntStream.rangeClosed(1, 10)
+				.mapToObj(x -> new Response(x * input))
+				.collect(Collectors.toList());
+		
 	}
 
 }

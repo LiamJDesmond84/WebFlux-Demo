96fddb9a8f2b6cb5b3cb9c9ac2dc83ea2896ab16
Liam Desmond <liamjdesmond84@gmail.com>
2022-09-25 16:11:09 -0500

    Adding findSquare method

--- webflux-demo/src/main/java/com/liam/webfluxdemo/services/MathService.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/services/MathService.java
@@ -2,9 +2,14 @@ package com.liam.webfluxdemo.services;
 
 import org.springframework.stereotype.Service;
 
+import com.liam.webfluxdemo.dtos.Response;
+
 @Service
 public class MathService {
 	
-	
+	public Response findSquare(int input) {
+		
+		return new Response(input * input);
+	}
 
 }

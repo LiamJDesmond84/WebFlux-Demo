b6eb264f9bf5927e5621d39da0cfffc5d52ab147
Liam Desmond <liamjdesmond84@gmail.com>
2022-09-25 16:09:04 -0500

    Updating Response DTO

--- webflux-demo/src/main/java/com/liam/webfluxdemo/dtos/Response.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/dtos/Response.java
@@ -2,9 +2,25 @@ package com.liam.webfluxdemo.dtos;
 
 import java.util.Date;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 public class Response {
 
 	private Date date = new Date();
 	
 	private int output;
+
+	public Response(int output) {
+
+		this.output = output;
+	}
+	
+	
+	
+	
+	
+	
 }

58505c6b141e4ac24ce06064338c660574d67c3e
Liam Desmond <liamjdesmond84@gmail.com>
2022-10-01 13:53:13 -0500

    Updating MathController

--- webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/MathController.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/MathController.java
@@ -1,5 +1,7 @@
 package com.liam.webfluxdemo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,11 +23,16 @@ public class MathController {
 	@GetMapping("square/{input}")
 	public Response findSquare(@PathVariable int input) {
 		
-		return this.mathService.findSquare(input);
+		return mathService.findSquare(input);
 	}
 	
 	
 	
+	@GetMapping("table/{input}")
+	public List<Response> multiplicationTable(@PathVariable int input) {
+		
+		return mathService.multiplicationTable(input);
+	}
 	
 	
 }

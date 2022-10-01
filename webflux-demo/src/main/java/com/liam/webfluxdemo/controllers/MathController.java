8ff2367a2422011ea85260fc8a97cb5ceb80fd3f
Liam Desmond <liamjdesmond84@gmail.com>
2022-10-01 13:51:04 -0500

    Adding MathController

---
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/MathController.java
@@ -0,0 +1,31 @@
+package com.liam.webfluxdemo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.liam.webfluxdemo.dtos.Response;
+import com.liam.webfluxdemo.services.MathService;
+
+@RestController
+@RequestMapping("math")
+public class MathController {
+
+	
+	@Autowired
+	private MathService mathService;
+	
+	
+	@GetMapping("square/{input}")
+	public Response findSquare(@PathVariable int input) {
+		
+		return this.mathService.findSquare(input);
+	}
+	
+	
+	
+	
+	
+}

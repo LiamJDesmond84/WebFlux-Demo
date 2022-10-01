d3864b5925700da7b52bc577376566ed2d9dff52
Liam Desmond <liamjdesmond84@gmail.com>
2022-10-01 14:26:19 -0500

    Updating ReactiveMathService

--- webflux-demo/src/main/java/com/liam/webfluxdemo/services/ReactiveMathService.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/services/ReactiveMathService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.liam.webfluxdemo.dtos.Response;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -12,7 +13,18 @@ public class ReactiveMathService {
 	
 	public Mono<Response> findSquare(int input) {
 		return Mono.fromSupplier(() -> input * input)
-				.map(x -> new Response(x));
+				.map(x -> new Response(x))
+				.log();
 //				.map(Response::new);
+	}
+	
+	
+	public Flux<Response> multiplicationTable(int input) {
+		
+		return Flux.range(1, 10)
+				.doOnNext(x -> SleepUtil.sleepSeconds(1))
+				.doOnNext(x -> System.out.println("Reactive MathService processing element: " + x))
+				.map(x -> new Response(x * input))
+				.log();
 	}
 }

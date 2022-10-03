fc2e24cb48050d5b47090d921a8105e36cd46952
Liam Desmond <liamjdesmond84@gmail.com>
2022-10-02 20:29:26 -0500

    Adding findSquare method

--- webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ReactiveMathController.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ReactiveMathController.java
@@ -1,6 +1,36 @@
 package com.liam.webfluxdemo.controllers;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.liam.webfluxdemo.dtos.Response;
+import com.liam.webfluxdemo.services.ReactiveMathService;
+
+@RestController
+@RequestMapping("reactive-math")
 public class ReactiveMathController {
+	
+	@Autowired
+	private ReactiveMathService reactiveMathService;
+	
+	
+	@GetMapping("square/{input}")
+	public Mono<Response> findSquare(@PathVariable int input) {
+		
+		return reactiveMathService.findSquare(input);
+	}
+	
+	
+	
+	@GetMapping("table/{input}")
+	public List<Response> multiplicationTable(@PathVariable int input) {
+		
+		return mathService.multiplicationTable(input);
+	}
 
 }

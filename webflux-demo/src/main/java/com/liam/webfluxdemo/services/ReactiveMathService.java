75891437293afb5b4df315d739b267af5d3dcbbc
Liam Desmond <liamjdesmond84@gmail.com>
2022-10-24 19:52:01 -0500

    Updating Flux multiplicationTable method

--- webflux-demo/src/main/java/com/liam/webfluxdemo/services/ReactiveMathService.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/services/ReactiveMathService.java
@@ -1,6 +1,9 @@
 package com.liam.webfluxdemo.services;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Service;
 
@@ -23,11 +26,18 @@ public class ReactiveMathService {
 	
 	public Flux<Response> multiplicationTable(int input) {
 		
-		return Flux.range(1, 10)
-				.delayElements(Duration.ofSeconds(1))
-//				.doOnNext(x -> SleepUtil.sleepSeconds(1))
-				.doOnNext(x -> System.out.println("Reactive MathService processing element: " + x))
-				.map(x -> new Response(x * input))
-				.log();
+		List<Response> list = IntStream.rangeClosed(1, 10)
+			.peek(x -> SleepUtil.sleepSeconds(1))
+			.peek(x -> System.out.println("MathService processing element: " + x))
+			.mapToObj(x -> new Response(x * input))
+			.collect(Collectors.toList());
+		return Flux.fromIterable(list);
+		
+//		return Flux.range(1, 10)
+//				.delayElements(Duration.ofSeconds(1))
+////				.doOnNext(x -> SleepUtil.sleepSeconds(1))
+//				.doOnNext(x -> System.out.println("Reactive MathService processing element: " + x))
+//				.map(x -> new Response(x * input))
+//				.log();
 	}
 }

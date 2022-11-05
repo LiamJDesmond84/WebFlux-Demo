0da2285fa054651d0c1894130445ba1c340f296d
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-05 13:19:22 -0500

    Added InputValidationException Class

--- webflux-demo/src/main/java/com/liam/webfluxdemo/exceptions/InputValidationException.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/exceptions/InputValidationException.java
@@ -1,5 +1,24 @@
 package com.liam.webfluxdemo.exceptions;
 
-public class InputValidationException {
+public class InputValidationException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static final String MSG = "Allowed range is 10 to 20";
+	
+	private static final int errorCode = 100;
+	
+	private final int input;
+
+	public InputValidationException(int input) {
+		super(MSG);
+		this.input = input;
+	}
+	
+	
+	
 
 }

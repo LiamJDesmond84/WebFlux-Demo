9301e721850cdcb653d00e836d10dac68395fd8d
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-06 17:49:34 -0600

    Adding notes

--- webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ReactiveMathValidationController.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ReactiveMathValidationController.java
@@ -1,10 +1,12 @@
 package com.liam.webfluxdemo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.liam.webfluxdemo.dtos.Response;
 import com.liam.webfluxdemo.exceptions.InputValidationException;
@@ -26,6 +28,18 @@ public class ReactiveMathValidationController {
 		if(input < 10 || input > 20) {
 			
 			throw new InputValidationException(input);
+			
+			// 1: @ControllerAdvice
+			// 2: InputValidationHandler
+			// 3: @ExceptionHandler(InputValidationException.class)
+			// 4: public ResponseEntity<InputFailedValidationResponse> handleException(InputValidationException exc)
+			
+			// 5: InputFailedValidationResponse RESPONSE = new InputFailedValidationResponse(); with 
+			
+			// 6: Set ResponseEntity Values
+			
+			// return ResponseEntity.badRequest().body(RESPONSE);
+			
 			
 		}
 		

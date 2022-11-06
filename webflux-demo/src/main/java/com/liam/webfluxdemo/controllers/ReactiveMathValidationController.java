4acce8a80a5a6c80bfb09855c23e423dbcf12e07
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-06 17:35:25 -0600

    Adding Validation controller

--- webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ReactiveMathValidationController.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ReactiveMathValidationController.java
@@ -1,5 +1,29 @@
 package com.liam.webfluxdemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.liam.webfluxdemo.dtos.Response;
+import com.liam.webfluxdemo.services.ReactiveMathService;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("reactive-math")
 public class ReactiveMathValidationController {
+	
+	@Autowired
+	private ReactiveMathService reactiveMathService;
+	
+	
+	// input * input
+	@GetMapping("square/{input}/throw")
+	public Mono<Response> findSquare(@PathVariable int input) {
+		
+		return reactiveMathService.findSquare(input);
+	}
 
 }

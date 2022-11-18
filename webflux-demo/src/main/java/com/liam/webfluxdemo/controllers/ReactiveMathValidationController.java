f6c8a5d40dd3a6d566504b8e5b5bc64c98b5c184
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-18 12:52:12 -0600

    Adding new error method

--- webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ReactiveMathValidationController.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ReactiveMathValidationController.java
@@ -26,7 +26,7 @@ public class ReactiveMathValidationController {
 	@GetMapping("square/{input}/throw")
 	public Mono<Response> findSquare(@PathVariable int input) {
 		if(input < 10 || input > 20) {
-			
+
 			throw new InputValidationException(input);
 			
 			// 1: @ControllerAdvice
@@ -47,29 +47,24 @@ public class ReactiveMathValidationController {
 		return reactiveMathService.findSquare(input);
 	}
 	
-	// input * input
-	@GetMapping("square/{input}/throw")
+	// Same as above
+	@GetMapping("square/{input}/mono-error")
 	public Mono<Response> monoError(@PathVariable int input) {
-		if(input < 10 || input > 20) {
-			
-			throw new InputValidationException(input);
-			
-			// 1: @ControllerAdvice
-			// 2: InputValidationHandler
-			// 3: @ExceptionHandler(InputValidationException.class)
-			// 4: public ResponseEntity<InputFailedValidationResponse> handleException(InputValidationException exc)
-			
-			// 5: InputFailedValidationResponse RESPONSE = new InputFailedValidationResponse(); with 
-			
-			// 6: Set ResponseEntity Values
-			
-			// return ResponseEntity.badRequest().body(RESPONSE);
-			
-			
-		}
 		
-		
-		return reactiveMathService.findSquare(input);
+		return Mono.just(input)
+				.handle((integer, sink) -> {
+					
+					if(integer >= 10 && integer <= 20) {
+						sink.next(integer);
+					}
+
+					else {
+						sink.error(new InputValidationException(integer));
+					
+					}})
+					.cast(Integer.class)
+					.flatMap(x -> reactiveMathService.findSquare(x));
+
 	}
 
 }

09a5d102129934793ece0f784b36c1be5a4afa10
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-20 16:34:43 -0600

    Adding notes

--- webflux-demo/src/main/java/com/liam/webfluxdemo/WebfluxDemoApplication.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/WebfluxDemoApplication.java
@@ -9,5 +9,14 @@ public class WebfluxDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxDemoApplication.class, args);
 	}
+	
+	
+	
+	
+	
+	// IMPORTANT
+		// Response - /math/table/{input} - Will not stop on cancel & will only return/collect, convert to JSON & return the items until it is completed.
+		// Response - /reactive-math/table/{input} - Will stop on cancel but will only return/collect, convert to JSON & return the items until it is completed. - Asynchronous/non-blocking
+		// Response - /reactive-math/table/{input}/stream - Can be cancelled anytime, will convert to JSON & return each item as it is emitted. JSON conversion for every single item. - Asynchronous/non-blocking
 
 }

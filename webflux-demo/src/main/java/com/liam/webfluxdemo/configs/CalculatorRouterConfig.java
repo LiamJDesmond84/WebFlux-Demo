2358b1aebe70715141b369d90e4685b7ff6b436a
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-24 20:19:15 -0600

    Get some.

--- webflux-demo/src/main/java/com/liam/webfluxdemo/configs/CalculatorRouterConfig.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/configs/CalculatorRouterConfig.java
@@ -1,7 +1,10 @@
 package com.liam.webfluxdemo.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -13,23 +16,44 @@ public class CalculatorRouterConfig {
 	@Autowired
 	CalculatorHandler calculatorHandler;
 	
-	
-//	public RouterFunction<ServerResponse> highLevelCalcRouter {
-//		
-//		return RouterFunctions.route()
-//				.path
-//	
-
-	
-	public RouterFunction<ServerResponse> serverResponseCalcRouterFunction() {
+	@Bean
+	public RouterFunction<ServerResponse> highLevelCalcRouter() {
 		
 		return RouterFunctions.route()
-			.GET("add/{a}/{b}", calculatorHandler::additionHandler)
-			.GET("sub/{a}/{b}", calculatorHandler::subtractionHandler)
-			.GET("mul/{a}/{b}", calculatorHandler::multiplicationHandler)
-			.GET("div/{a}/{b}", calculatorHandler::divisionHandler)
+				.path("calculator", this::serverResponseCalcRouterFunction)
+				.build();
+	}
+
+	
+	private RouterFunction<ServerResponse> serverResponseCalcRouterFunction() {
+		
+		return RouterFunctions.route()
+			.GET("{a}/{b}", isOperation("+"), calculatorHandler::additionHandler)
+			.GET("{a}/{b}", isOperation("-"), calculatorHandler::subtractionHandler)
+			.GET("{a}/{b}", isOperation("*"), calculatorHandler::multiplicationHandler)
+			.GET("{a}/{b}", isOperation("/"), calculatorHandler::divisionHandler)
 			.build();
 			
 	}
+	
+	private RequestPredicate isOperation(String operation) {
+		
+		return RequestPredicates.headers(header -> operation.equals(header.asHttpHeaders()
+													.toSingleValueMap().get("OP")));
+		
+	}
+	
+	
+	
+//	public RouterFunction<ServerResponse> serverResponseCalcRouterFunction() {
+//		
+//		return RouterFunctions.route()
+//			.GET("add/{a}/{b}", calculatorHandler::additionHandler)
+//			.GET("sub/{a}/{b}", calculatorHandler::subtractionHandler)
+//			.GET("mul/{a}/{b}", calculatorHandler::multiplicationHandler)
+//			.GET("div/{a}/{b}", calculatorHandler::divisionHandler)
+//			.build();
+//			
+//	}
 	
 }

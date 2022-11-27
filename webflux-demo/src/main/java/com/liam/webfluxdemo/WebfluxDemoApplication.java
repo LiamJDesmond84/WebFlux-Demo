a0dad73cf64357750a0640e7065538032820e6a0
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-26 19:03:24 -0600

    Adding BaseTest

--- webflux-demo/src/main/java/com/liam/webfluxdemo/WebfluxDemoApplication.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/WebfluxDemoApplication.java
@@ -2,7 +2,7 @@ package com.liam.webfluxdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
+
 
 @SpringBootApplication
 public class WebfluxDemoApplication {

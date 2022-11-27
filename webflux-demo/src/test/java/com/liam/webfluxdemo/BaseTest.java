dac32c4cb65b9b9aaa8c8fa1f341e76e8583bec8
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-26 19:09:33 -0600

    Added WebClient Bean

--- webflux-demo/src/test/java/com/liam/webfluxdemo/BaseTest.java
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/BaseTest.java
@@ -1,9 +1,10 @@
 package com.liam.webfluxdemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class BaseTest {
 
 
---
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/L01GetSingleResponseTest.java
@@ -0,0 +1,11 @@
+package com.liam.webfluxdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class L01GetSingleResponseTest extends BaseTest {
+	
+	@Autowired
+	private WebClient webClient;
+
+}

3246f21c5e69a39c7c6aa290bed0d74a3357afb7
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-26 19:28:46 -0600

    Added WebClient base build

--- webflux-demo/src/test/java/com/liam/webfluxdemo/L01GetSingleResponseTest.java
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/L01GetSingleResponseTest.java
@@ -1,11 +1,28 @@
 package com.liam.webfluxdemo;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import com.liam.webfluxdemo.dtos.Response;
 
 public class L01GetSingleResponseTest extends BaseTest {
 	
 	@Autowired
 	private WebClient webClient;
+	
+	
+	@Test
+	public void blockTest() {
+		
+		
+		webClient
+			.get()
+			.uri("reactive-math/square/{inputVar}", 5)
+			.retrieve()
+			.bodyToMono(Response.class) // Mono<Response>
+			.block();
+		
+	}
 
 }
--- webflux-demo/src/test/java/com/liam/webfluxdemo/configs/WebClientConfig.java
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/configs/WebClientConfig.java
@@ -10,6 +10,8 @@ public class WebClientConfig {
 	@Bean
 	public WebClient webClient() {
 		
-		return WebClient.builder().build();
+		return WebClient.builder()
+				.baseUrl("http://localhost:8080")
+				.build();
 	}
 }

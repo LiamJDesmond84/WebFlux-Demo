a4d7405be739aefca173e8c3742e95f07df416a2
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-28 16:21:01 -0600

    Added buiRequestDto

--- webflux-demo/src/test/java/com/liam/webfluxdemo/L03PostRequestTest.java
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/L03PostRequestTest.java
@@ -1,7 +1,11 @@
 package com.liam.webfluxdemo;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.server.ServerRequest;
+
+import com.liam.webfluxdemo.dtos.MultiplyRequestDto;
 
 public class L03PostRequestTest extends BaseTest {
 	
@@ -9,5 +13,24 @@ public class L03PostRequestTest extends BaseTest {
 	private WebClient webClient;
 	
 	
+	@Test
+	public void postTest(ServerRequest serverRequest) {
+		
+		Mono<Response> monoResponse = webClient
+				.post()
+				.uri()
+		
+	}
+	
+	
+	private MultiplyRequestDto buiRequestDto(int a, int b) {
+		
+		MultiplyRequestDto dto = new MultiplyRequestDto();
+		
+		dto.setFirst(a);
+		dto.setSecond(b);
+		
+		return dto;
+	}
 
 }

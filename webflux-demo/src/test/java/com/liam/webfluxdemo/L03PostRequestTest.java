21078fc044e71337198f4a59f4e9fbddbbcb1438
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-28 16:17:10 -0600

    Added L03PostRequestTest

---
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/L03PostRequestTest.java
@@ -0,0 +1,13 @@
+package com.liam.webfluxdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class L03PostRequestTest extends BaseTest {
+	
+	@Autowired
+	private WebClient webClient;
+	
+	
+
+}

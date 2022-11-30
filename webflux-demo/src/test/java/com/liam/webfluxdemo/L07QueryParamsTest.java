ab27dd65bfdb2a2668b82a8b36fca5f009f9dc72
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-29 22:17:53 -0600

    Adding queryParamsTest

--- webflux-demo/src/test/java/com/liam/webfluxdemo/L07QueryParamsTest.java
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/L07QueryParamsTest.java
@@ -1,5 +1,23 @@
 package com.liam.webfluxdemo;
 
-public class L07QueryParamsTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class L07QueryParamsTest extends BaseTest {
+	
+	
+	@Autowired
+	private WebClient webClient;
+	
+	String queryString = "http://localhost:8080/jobs/search?count={count}&page={page}";
+	
+	
+	@Test
+	public void queryParamsTest() {
+		
+		UriComponentsBuilder.fromUriString(queryString);
+	}
 
 }

5b1d128bb588d2f74ad1625365476d1dcc9469b3
Liam Desmond <liamjdesmond84@gmail.com>
2022-11-29 22:13:20 -0600

    Adding method for queryParams

--- webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ParamsController.java
+++ webflux-demo/src/main/java/com/liam/webfluxdemo/controllers/ParamsController.java
@@ -1,8 +1,19 @@
 package com.liam.webfluxdemo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("jobs")
 public class ParamsController {
 
+	
+	@GetMapping("/search")
+	public void searchJobs(@RequestParam("count") int count, @RequestParam("age") int age) {
+		
+		
+		
+	}
 }

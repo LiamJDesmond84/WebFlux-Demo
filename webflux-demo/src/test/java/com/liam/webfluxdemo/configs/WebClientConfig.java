8516000aa1999b2a9c4380d39b5a2b5bc114cc2a
Liam Desmond <liamjdesmond84@gmail.com>
2022-12-03 20:12:19 -0600

    Adding ExchangeFunction generator

--- webflux-demo/src/test/java/com/liam/webfluxdemo/configs/WebClientConfig.java
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/configs/WebClientConfig.java
@@ -2,7 +2,12 @@ package com.liam.webfluxdemo.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.ExchangeFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class WebClientConfig {
@@ -14,5 +19,13 @@ public class WebClientConfig {
 				.baseUrl("http://localhost:8080")
 				.defaultHeaders(h -> h.setBasicAuth("username", "password"))
 				.build();
+	}
+	
+	
+	private Mono<ClientResponse> sessionToken(ClientRequest request, ExchangeFunction exFunc) {
+		
+		System.out.println("Generating Session");
+		
+		
 	}
 }

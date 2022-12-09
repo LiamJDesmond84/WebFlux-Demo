ec7a9b1d1f88236434d31c6cef4f7c5c071b7a3d
Liam Desmond <liamjdesmond84@gmail.com>
2022-12-09 13:13:25 -0600

    Added new Test class

--- webflux-demo/src/test/java/com/liam/webfluxdemo/L09AssignmentTest.java
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/L09AssignmentTest.java
@@ -5,9 +5,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 public class L09AssignmentTest extends BaseTest {
 	
+	private static final String FORMAT = "%d %s %d = %s";
+	
+	private static final int A = 10;
+	
 	
 	
 	@Autowired
 	private WebClient webClient;
 
+	
+	
 }

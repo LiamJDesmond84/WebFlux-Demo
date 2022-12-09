6470d54b1fe0f98075701c8725854f2ce29be89a
Liam Desmond <liamjdesmond84@gmail.com>
2022-12-08 19:09:52 -0600

    Adding AttributesTest

---
+++ webflux-demo/src/test/java/com/liam/webfluxdemo/L08AttributesTest.java
@@ -0,0 +1,57 @@
+package com.liam.webfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.liam.webfluxdemo.dtos.MultiplyRequestDto;
+import com.liam.webfluxdemo.dtos.Response;
+
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class L08AttributesTest extends BaseTest {
+	
+	
+	@Autowired
+	private WebClient webClient;
+	
+	String queryString = "http://localhost:8080/jobs/search?count={count}&page={page}";
+	
+	@Test
+	public void headersTest() {
+		
+		Mono<Response> responseMono = webClient
+			.post()
+			.uri("reactive-math/multiply")
+			.headers(h -> h.set("someKey", "someValue"))
+//			.headers(h -> h.setBasicAuth("username", "password"))
+			.header("someKey2", "someValue2")
+			.bodyValue(buildRequestDto(5, 2))
+			.retrieve()
+			.bodyToMono(Response.class)
+			.doOnNext(System.out::println);
+		
+		StepVerifier.create(responseMono) // Session Token Generator invoked individually
+			.expectNextCount(1)
+			.verifyComplete();
+		
+		StepVerifier.create(responseMono) // Session Token Generator invoked individually
+			.expectNextCount(1)
+			.verifyComplete();
+		
+		
+	}
+	
+	
+	private MultiplyRequestDto buildRequestDto(int a, int b) {
+		
+		MultiplyRequestDto dto = new MultiplyRequestDto();
+		
+		dto.setFirst(a);
+		dto.setSecond(b);
+		
+		return dto;
+	}
+
+}
